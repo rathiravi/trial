e61b3b5d954160b293c67a38b7b8827ead5eadea
rathiravi <rathiabhinaya@gmail.com>
2016-07-07 11:16:14 -0700

    Day 1 Java bootcamp

---
+++ JavaBootcampDay2Zeller/src/javabootcampday2zeller/JavaBootcampDay2Zeller.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javabootcampday2zeller;
+import java.util.Scanner;
+/**
+ *
+ * @author RATHRAVI
+ */
+public class JavaBootcampDay2Zeller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter quit to");
+        String inCmd = in.nextLine();
+        do{
+            
+        }while(inCmd != "quit");
+        
+    }
+    
+}
