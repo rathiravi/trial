692d45532f67ef6d1f5a5d390b289d43546e81bf
rathiravi <rathiabhinaya@gmail.com>
2016-07-09 15:44:30 -0700

    Day 3 complete

---
+++ MethodArguments/src/methodarguments/MethodArguments.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package methodarguments;
+
+/**
+ *
+ * @author RATHRAVI
+ */
+public class MethodArguments {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static int addUp(int[] args){
+        System.out.println("Executing int addition");
+        int sum = 0;
+        if(args.length == 0 || args.length ==1)
+            sum = 0;
+        else{
+            for (int i = 0; i<args.length; i++){
+                sum += args[i];
+            }
+        }
+        return sum;
+    }
+    
+    public static double addUp(double[] args){
+        System.out.println("Executing double addition");
+        double sum = 0;
+        if(args.length == 0 || args.length ==1)
+            sum = 0;
+        else{
+            for (int i = 0; i<args.length; i++){
+                sum += args[i];
+            }
+        }
+        return sum;
+    }
+    
+    public static int addUp(String[] args){
+        System.out.println("Executing double addition");
+        int sum = 0;
+        if(args.length == 0 || args.length ==1)
+            sum = 0;
+        else{
+            for (int i = 0; i<args.length; i++){
+                sum += toNumber(args[i]);
+            }
+        }
+        return sum;
+    }
+    
+    static String numString[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",};
+
+    public static int toNumber(String numText){
+        for(int i = 0; i< numString.length; i++){
+            if(numString[i].equals(numText))
+                return i;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String arguments[] = {"one","three"};
+        System.out.println(addUp(arguments)); 
+//        System.out.println(addUp(1, 4));
+//        System.out.println(addUp("one", "three"));
+//        System.out.println(addUp(2, 3.5));
+//        System.out.println(addUp(1, "three"));
+
+
+//        public static int addUp(int i, int j){
+//        System.out.println("Executing int addition");
+//        return i+j;
+//    }
+//    
+//    public static double addUp(double i, double j){
+//        System.out.println("Executing double addition");
+//        return i+j;
+//    }
+//    
+//    public static int addUp(String i, String j){
+//        System.out.println("Executing double addition");
+//        return (toNumber(i) + toNumber(j));
+//    }
+    
+    
+    }
+    
+}
